6c0df00580c33088ec744aa24e2dac5bdbe3ff98
Emyfony <emeline.fourny@gmail.com>
2018-09-11 15:26:30 +0200

    Update Secretaire FINISH FileAttente OK Sf liens /MED + /PATIENT

--- hopital/src/main/java/sopra/promo404/hopital/model/FileAttente.java
+++ hopital/src/main/java/sopra/promo404/hopital/model/FileAttente.java
@@ -1,36 +1,64 @@
 package sopra.promo404.hopital.model;
 
-import java.util.ArrayList;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Version;
+@Entity
 public class FileAttente {
+	@Id
+	@GeneratedValue
 	private Long id;
+	@Version
+	private int version;
+	@Column
+	private int capacite;
+
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="secretaire_id")
+	private Secretaire secretaire;
+	
+	
+	
+	public FileAttente() {
+		super();
+	}
+	
+	
+	
+	public FileAttente(Long id, int version, int capacite) {
+		super();
+		this.id = id;
+		this.version = version;
+		this.capacite = capacite;
+	}
+
+
 
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
-	private ArrayList<Patient> patients;
-
-	public ArrayList<Patient> getPatients() {
-		return patients;
+	public int getVersion() {
+		return version;
+	}
+	public void setVersion(int version) {
+		this.version = version;
+	}
+	public int getCapacite() {
+		return capacite;
+	}
+	public void setCapacite(int capacite) {
+		this.capacite = capacite;
 	}
 	
-	public void addPatient(Patient patient) {
-		this.patients.add(patient);
-	}
-
-	public void setPatients(ArrayList<Patient> patients) {
-		this.patients = patients;
-	}
-
-	public FileAttente() {
-		super();
-		this.patients = new ArrayList<Patient>();
-	}
-		
 	
 }
--- hopital/src/main/java/sopra/promo404/hopital/model/Secretaire.java
+++ hopital/src/main/java/sopra/promo404/hopital/model/Secretaire.java
@@ -1,15 +1,23 @@
 package sopra.promo404.hopital.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
 
 @Entity
 public class Secretaire {
 	@Id
 	@GeneratedValue
 	private Long id;
+	@Version
+	private int version;
 	@Column(length = 100)
 	private String nom;
 
@@ -19,9 +27,21 @@ public class Secretaire {
 	private Civilite civilite;
 	@Column
 	private boolean enPause;
+	@Column
+	@OneToMany(mappedBy = "Secretaire", fetch=FetchType.LAZY)
+	private List<FileAttente> fileAttentes = new ArrayList<>();
 
 	public Secretaire() {
 		super();
+	}
+
+	public Secretaire(Long id, String nom, String prenom, Civilite civilite, boolean enPause) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.civilite = civilite;
+		this.enPause = enPause;
 	}
 
 	public Long getId() {
